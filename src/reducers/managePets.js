92ab66f207b280cb2684516adf94b2df70f24087
Luke Ghenco <lukeghenco@gmail.com>
2017-02-07 12:00:59 -0500

    update solution to not use action.payload

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,38 +1,40 @@
 export let state;
 
-let defaultState = {pets: []};
-
-export function managePets(state = defaultState, action){
+export const managePets = (state = {
+  pets: [],
+}, action) => {
   switch(action.type) {
     case "ADD_PET":
-      let pet = action.payload;
-      return {...state, pets: [...state.pets, pet]};
+      return {
+        ...state,
+        pets: [
+          ...state.pets,
+          action.pet
+        ]
+      };
     case "REMOVE_PET":
-      let indexToRemove = state.pets
-        .findIndex(pet => pet.id === action.payload);
-
-      let pets = [
-        ...state.pets.slice(0, indexToRemove),
-        ...state.pets.slice(indexToRemove + 1)
-      ];
-
-      return {...state, pets: pets};
+      const petIndex = state.pets.findIndex(pet => pet.id === action.id);
+      return {
+        ...state,
+        pets: [
+          ...state.pets.slice(0, petIndex),
+          ...state.pets.slice(petIndex + 1)
+        ]
+      };
     default:
       return state;
   }
 }
 
-export function dispatch(action){
-  state = managePets(state, action)
-  render()
-}
+export const dispatch = (action) => {
+  state = managePets(state, action);
+  render();
+};
 
-export function render(){
-  let container = document.getElementById('container')
-  let petsLis = state.pets.map(function(pet){
-    return `<li>${pet.name}</li>`
-  })
-  let joinedLis = petsLis.join(' ')
-  container.innerHtml = `<ul>${joinedLis}</ul>`
-
+export const render = () => {
+  let container = document.getElementById('container');
+  let petsList = state.pets.map((pet) => {
+    return `<li>${pet.name}</li>`;
+  }).join(' ');
+  container.innerHTML = `<ul>${petsList}</ul>`;
 }
