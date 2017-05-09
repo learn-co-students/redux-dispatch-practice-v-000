e1c5956a59e42b189896a1fdb144f1d7e9304ada
ldgarber <leah.garber88@gmail.com>
2017-05-09 16:36:17 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,29 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state={pets: []}, action){
+  switch (action.type) {
+    case "ADD_PET": 
+      return {pets: state.pets.concat(action.pet)}; 
+    case "REMOVE_PET": 
+      var pets = state.pets.filter(function(pet) {
+        return pet.id !== action.id 
+      }); 
+      return {pets: pets}; 
+    default: 
+      return state; 
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action); 
+  render();
 }
 
 export function render(){
-
+  var display = "<ul>"; 
+  for (var i = 0; i < state.pets.length; i++) {
+    display = display + "<li>"+ state.pets[i].name + "</li>"; 
+  }
+  display = display + "</ul>"
+  document.getElementById('container').innerHTML = display; 
 }
