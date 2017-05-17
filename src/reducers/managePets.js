45fa53a819eed0161a3412d4c3477eeee3099db4
Idayat <github email address>
2017-05-16 22:24:24 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,38 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return {
+        ...state,
+        pets: [
+          ...state.pets, action.pet
+        ]
+      }
+      case 'REMOVE_PET':
+        const petIndex = state.pets.findIndex(pet => pet.id === action.id)
+        return Object.assign({}, state,{
+          pets: [
+            ...state.pets.slice(0, petIndex),
+            ...state.pets.slice(petIndex +1)
+          ]
+        })
+    default:
+        return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
-export function render(){
+export const render = () => {
 
+let container = document.getElementById('container');
+let petList = state.pets.map((pet) =>{
+  return `<li>${pet.name}</li>`;
+}).join(' ');
+  container.innerHTML = `<ul>${petList}</ul>`
 }
