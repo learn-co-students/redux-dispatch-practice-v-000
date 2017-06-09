9f60565ef2ebc5d71887f6408ab96204061969a7
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-06-09 11:29:57 -0700

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,25 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+	switch(action.type){
+		case 'ADD_PET':
+			return {pets: [...state.pets, action.pet]}
+		case 'REMOVE_PET':
+			var index = state.pets.map(pet => {return pet.id}).indexOf(action.id)
+			return {pets:[ ...state.pets.slice(0, index), ...state.pets.slice(index+1)]}
+		default:
+			return state;
+	}
 }
 
-export function dispatch(){
+export function dispatch(action){
+	state = managePets(state, action)
+	render()
 }
 
 export function render(){
-
+	var container = document.getElementById('container');
+ 
 }
+
+dispatch({type: '@@INIT'})
