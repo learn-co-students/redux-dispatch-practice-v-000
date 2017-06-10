7cbb0f0dbc06f9a7762403c2a2fbdc125057e993
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-10 13:15:45 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -5,16 +5,27 @@ export function managePets(state = {pets: []},action){
     let newPets = state.pets.splice();
     switch(action.type) {
         case "ADD_PET":
-            newPets = [...state.pets, action.pet]
-            return {...state, pets: newPets}
-        case "REMOVE_PET":
-            console.log("REMOVING PET")
+
+            return {
+                ...state,
+                pets: [
+                ...state.pets,
+                action.pet
+                ]
+            }
+            case "REMOVE_PET":
             let indexToRemove = state.pets.findIndex( pet => {
                 return pet.id === action.id
             })
-            newPets = newPets.splice(indexToRemove,1);
-            return {...state, pets: newPets}
-        default:
+
+            return {
+                ...state, 
+                pets: [
+                ...state.pets.slice(0, indexToRemove),
+                ...state.pets.slice(indexToRemove + 1)
+                ]
+            }
+            default:
             return state;
     }
 }
@@ -28,6 +39,6 @@ export function render(){
     let petsList = state.pets.reduce( (list, pet) => {
         return list + "<li>" + pet.name + "</li>"
     }, '')
-    petsList = "<div id='container'><ul>" + petsList + "</ul></div>"
-    document.getElementById('main').setInnerHTML = petsList;
+    petsList = "<ul>" + petsList + "</ul>"
+    document.getElementById('container').innerHTML = petsList;
 }
