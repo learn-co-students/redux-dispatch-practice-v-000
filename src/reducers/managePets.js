b787b0895da405544abf3bc1a07cf8c63a982527
github username <kevinladkins@gmail.com>
2017-06-13 18:07:51 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,35 @@
+
 export let state;
 
+export function managePets(state = {pets: [],}, action) {
+  switch (action.type) {
+    case 'ADD_PET':
+      let newState = Object.assign({}, state, {});
+      newState.pets.push(action.pet);
+      return newState
+    case 'REMOVE_PET':
+      let removalIndex = state.pets.findIndex(pet => pet.id === action.id)
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets.slice(0, removalIndex),
+          ...state.pets.slice(removalIndex +1)
+        ]
+      });
+    default:
+      return state;
+  }
+};
 
-export function managePets(){
-}
-
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action);
+  render();
 }
 
 export function render(){
-
+  var lis = state.pets.map(pet =>
+    `<li>${pet.name}</li>`
+  ).join(' ');
+  var list = "<ul>" + lis + "</ul>";
+  var container = document.getElementById("container");
+  container.innerHTML = list
 }
