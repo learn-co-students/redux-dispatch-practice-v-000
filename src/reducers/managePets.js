1e945bcfbc19619728127e01fe70f35548306368
Hima Chitalia <hima_chhag@yahoo.com>
2017-08-12 23:06:30 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,26 @@
 export let state;
 
+export function managePets(state = { pets: [] }, action){
+ switch (action.type) {
+   case 'ADD_PET':
+     return {pets: [...state.pets, action.pet]}
 
-export function managePets(){
+   case 'REMOVE_PET':
+     let removePetId = action.id
+     let removePets = state.pets.filter(function(p){return p.id !== removePetId})
+     return {pets: removePets}
+   default:
+     return state;
+ }
 }
 
-export function dispatch(){
+export function dispatch(action){
+ state = managePets(state, action)
+ render()
 }
 
 export function render(){
-
+ let lineItems = state.pets.map( p => `<li>${p.name}</li>`)
+ let html = `<ul>${lineItems.join('')}</ul>`
+ document.getElementById('container').innerHTML = html
 }
