51ad1825ce5ebc5cee966991855e0a06487389ba
christinechoi <smi77y@gmail.com>
2017-10-16 14:14:57 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,33 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = { pets: [] }, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return {pets: state.pets}
+    case 'REMOVE_PET':
+      return {}
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+
+
+//pass an action to the reducer, use return value to update the state
+export function dispatch(action){ 
+  state = managePets(state, action)
+  render();
 }
+
+
 
 export function render(){
+  document.innerHTML = state.count
 
+  // <div>
+  //   <ul id="container">
+  //     <li>pet name</li>
+  //   </ul>
+  // </div>
 }
