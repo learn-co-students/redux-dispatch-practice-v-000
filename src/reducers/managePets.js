fd8153d9c0adf10245ed041654151ee24ada01ea
Han Lee <hanlee12@gmail.com>
2017-11-09 17:31:31 -0500

    done

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,37 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state={ pets: [] }, action){
+
+  switch (action.type) {
+    case 'ADD_PET':
+      return {pets: [...state.pets, action.pet]}
+    case 'REMOVE_PET':
+      return {pets: state.pets.filter((pet) => (pet.id !== action.id))}
+    default:
+      return state;
+  }
+
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
+  // let petsString;
+  // if (state.pets.length) {
+  //   const petMapString = state.pets.map((pet)=> {
+  //     return `<li>${pet.name}<li>`
+  //   })
+  //   petsString = '<ul>' + petMapString.join('') + '</ul>'
+  //   //console.log(petsString)
+  // }
+  // document.getElementById('container').innerHTML = petsString || ''
+  const petsString = (state.pets.length) ? `<ul><li>${state.pets[0].name}</li></ul>` : ''
+  document.getElementById('container').innerHTML = petsString
 
 }
+
+// dispatch({type: '@@INIT'})
