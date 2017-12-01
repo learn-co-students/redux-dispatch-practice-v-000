80e98dc01d920814d2c8fd011d74a0b378fb52a1
WilliamMena <williammena1991@gmail.com>
2017-11-30 21:53:01 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,36 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = { pets: [] }, action){
+
+  switch (action.type) {
+
+    case 'ADD_PET':
+      // return { pets: [...state.pets, action.pet]}
+      return Object.assign({}, state, {pets: [...state.pets, action.pet]})
+
+
+    case 'REMOVE_PET':
+      // return --- something revolving slice. Find the index of the object you want to remove. Slice the array up until that point. Then slice everything after that.
+      let removeIndex = state.pets.findIndex(pet => pet.id === action.id);
+      return Object.assign({}, state, {pets: [
+        ...state.pets.slice(0, removeIndex),
+        ...state.pets.slice(removeIndex + 1)
+      ]})
+
+
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let petHTML = state.pets.map(pet => `<li>${pet.name}</li>`)
+  let petHTMLInList = `<ul>${petHTML}</ul>`
+  document.getElementById('container').innerHTML = petHTMLInList
 }
