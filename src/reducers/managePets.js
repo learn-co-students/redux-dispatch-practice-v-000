63a82afbba642078c95de96e6e1e13036137a4a3
Bruce Molina <bruce.molina.81@gmail.com>
2017-12-20 01:47:39 +0000

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,37 @@
 export let state;
+  //
+  // pet: { name: 'avalanche', species: 'puppy', id: 101 } }
+  // { pets: [ { name: 'Splash', type: 'turtle', id: 100 }, { name: 'avalanche', species: 'puppy', id: 101 } ] }
+export function managePets(state = {pets:[]}, action){
+  switch(action.type){
+      case 'ADD_PET':
+        return {
+          ...state,
+            pets:[
+              ...state.pets,
+              action.pet
+            ]};
+      case 'REMOVE_PET':
+        return {
+          ...state,
+          pets:
+            state.pets.filter( (pet) => pet.id !== action.id)
+        }
+      default:
+      return state;
+  }
 
-
-export function managePets(){
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let container = document.getElementById('main');
+ let petsList = state.pets.map((pet) => {
+   return `<li>${pet.name}</li>`;
+ }).join(' ');
+ container.innerHTML = `<ul>${petsList}</ul>`;
 }
