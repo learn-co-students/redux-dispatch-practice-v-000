2d0b406c457d6e071204b44f72a8bb467ecb0fb0
Bruce Molina <bruce.molina.81@gmail.com>
2017-12-21 03:09:04 +0000

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,23 +1,22 @@
 export let state;
-  //
-  // pet: { name: 'avalanche', species: 'puppy', id: 101 } }
-  // { pets: [ { name: 'Splash', type: 'turtle', id: 100 }, { name: 'avalanche', species: 'puppy', id: 101 } ] }
-export function managePets(state = {pets:[]}, action){
+
+
+export function managePets(state = {pets:[]},action){
   switch(action.type){
-      case 'ADD_PET':
+    case 'ADD_PET':
+      return {
+        ...state,
+        pets:[
+          ...state.pets,
+          action.pet
+        ]
+      }
+    case 'REMOVE_PET':
         return {
-          ...state,
-            pets:[
-              ...state.pets,
-              action.pet
-            ]};
-      case 'REMOVE_PET':
-        return {
-          ...state,
           pets:
-            state.pets.filter( (pet) => pet.id !== action.id)
+          state.pets.filter( (pet) =>  pet.id !== action.id)
         }
-      default:
+    default:
       return state;
   }
 
@@ -28,10 +27,10 @@ export function dispatch(action){
   render()
 }
 
-export function render(){
-  let container = document.getElementById('main');
- let petsList = state.pets.map((pet) => {
-   return `<li>${pet.name}</li>`;
- }).join(' ');
- container.innerHTML = `<ul>${petsList}</ul>`;
+export const render = () => {
+  let container = document.getElementById('container');
+  let petsList = state.pets.map((pet) => {
+    return `<li>${pet.name}</li>`;
+  }).join(' ');
+  container.innerHTML = `<ul>${petsList}</ul>`;
 }
