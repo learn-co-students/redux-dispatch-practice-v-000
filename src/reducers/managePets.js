a082df41d226f54721ee4dd1cee653e0164ed2b1
Nicholas D'Amico <nickalan82@icloud.com>
2018-01-26 15:20:30 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,37 @@
 export let state;
 
 
-export function managePets(){
+// Reducer Function
+export function managePets(state = { pets: [] }, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets,
+          action.pet
+        ]
+      });
+    case 'REMOVE_PET':
+      let petIndexNum = state.pets.map(p => p.id).indexOf(action.id);
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets.slice(0, petIndexNum),
+          ...state.pets.slice(petIndexNum + 1)
+        ]
+      });
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render();
 }
 
 export function render(){
-
+  let petList = '<ul>';
+        petList += state.pets.map(p => `<li>${p.name}</li>`);
+        petList += '</ul>';
+  document.getElementById('container').innerHTML = petList;
 }
