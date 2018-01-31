f2bb367c186c45868468de0cd5f34f868981ea07
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-01-31 23:28:09 +0000

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,24 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state= {pets: []}, action){
+    switch (action.type) {
+ 
+        case 'ADD_PET':
+            return Object.assign({}, state, { pets: [...state.pets, action.pet] })
+        case 'REMOVE_PET': 
+            let filteredPets = state.pets.filter( pet => pet.id !== action.id )
+            return Object.assign({}, state, {pets: [...filteredPets]}) 
+        default: 
+            return state;
+    }
 }
 
-export function dispatch(){
+export function dispatch(action){ 
+    state = managePets(state, action)
+    render()
 }
 
-export function render(){
-
+export function render(){ 
+    document.getElementById("container").innerHTML = `<ul><li>${state.pets.map(pet => pet.name)}</li></ul>`
 }
