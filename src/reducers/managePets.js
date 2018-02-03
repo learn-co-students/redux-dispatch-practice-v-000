71c43cea2fd9344bcfe5152b2b1f91a5ed236a65
gnappo1 <gnappo.1@live.it>
2018-02-03 01:10:50 -0800

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,40 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = { pets: [] }, action){
+  switch (action.type) {
+
+    case 'ADD_PET':
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets,
+          action.pet
+        ]
+      });
+
+    case 'REMOVE_PET':
+
+      const indexToRemove = state.pets.findIndex(pet => pet.id === action.id);
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets.slice(0, indexToRemove),
+          ...state.pets.slice(indexToRemove + 1)
+        ]
+      });
+
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let petList = state.pets.map(pet => {
+    return `<li>${pet.name}</li>`
+  })
+  document.getElementById("container").innerHTML = `<ul>${petList}</ul>`
 }
