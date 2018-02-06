37a68535641219b72fdc69053a3f9648bb6fe9b3
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-06 14:04:44 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,32 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+	let pets = state.pets;
+	switch (action.type) {
+    	case 'ADD_PET':
+    		if(action.pet["name"]) pets.push(action.pet);
+      		return {pets: pets } 
+	    case 'REMOVE_PET':
+	    	let newPets = pets.filter(function (el) {
+ 				return el.id !== action.id
+			});
+	      	return {pets: newPets}
+      	default:
+        	return state;
+    }
 }
 
-export function dispatch(){
+export function dispatch(action){
+	state = managePets(state, action)
+	render()
 }
 
 export function render(){
-
+	let output = "<ul>";
+	for (var i = 0; i < state.pets.length; i++) {
+		output += "<li>" + state.pets[i].name + "</li>";
+	}
+	output += "</ul>"
+	document.getElementById("container").innerHTML = output;
 }
