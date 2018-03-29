aaccc41d16a1a63a3b597d86f79b81c4ebb24ce7
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-29 16:48:28 +0200

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,25 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = { pets: [] }, action){
+  switch(action.type) {
+    case 'ADD_PET':
+      return {pets: [...state.pets, action.pet]}
+    case 'REMOVE_PET':
+      return {pets: [...state.pets.filter(p => p.id !== action.id)]}
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  document.getElementById('container').innerHTML = '<ul></ul>'
+  state.pets.forEach((pet) => {
+    document.getElementsByTagName('ul')[0].innerHTML += `<li>${pet.name}</li>`
+  })
 }
