67cf2ef43d5f9009fb9ee0af8f69dc661f0184e2
Alexander Urizar <urizaraj@gmail.com>
2018-04-16 12:53:19 -0400

    complete lab

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,30 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state={pets: []}, action) {
+  let pets
+
+  switch (action.type) {
+    case ('ADD_PET'):
+      pets = [...state.pets, action.pet]
+      return {...state, pets}
+
+    case ('REMOVE_PET'):
+      pets = state.pets.filter(p => p.id !== action.id)
+      return {...state, pets}
+
+    default: return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action) {
+  state = managePets(state, action)
+  render()
 }
 
-export function render(){
-
+export function render() {
+  const container = document.getElementById('container')
+  const pets = state.pets.map(pet => `<li>${pet.name}</li>`).join('')
+  const html = `<ul>${pets}</ul>`
+  container.innerHTML = html
 }
