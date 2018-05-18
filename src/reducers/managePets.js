a27f75d6498d4d0e86a647f3bcccdd9f8ec1dd13
ingridwong0715 <ingrid830715@gmail.com>
2018-05-18 16:49:15 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,25 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type){
+    case "ADD_PET":
+      return {pets: [...state.pets, action.pet]}
+    case "REMOVE_PET":
+      let newList = state.pets.filter(pet => pet.id !== action.id)
+      return {pets: newList}
+    default:
+      return {pets: [...state.pets]}
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action);
+  render();
 }
 
 export function render(){
+  let strings = state.pets.map(pet => `<li>${pet.name}</li>`).join(' ');
+  let html = `<ul>${strings}</ul>`
+  document.getElementById('container').innerHTML = html;
 
 }
