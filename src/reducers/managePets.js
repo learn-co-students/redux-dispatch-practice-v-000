78fadca0ad15b2f5a4e857affb21fd4700c9fafa
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-23 11:12:15 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,32 +1,32 @@
 export let state;
 
 
-export function managePets(state = {pets: []}, action){
+export const managePets = (state = { pets: [], }, action) => {
 	switch (action.type) {
-		case 'ADD_PET':
-			return Object.assign({}, state, {
-				pets: [...state.pets, action.pet]
-			});
-		case 'REMOVE_PET':
+		case "ADD_PET":
+			return { ...state, pets: [...state.pets, action.pet]
+			};
+		case "REMOVE_PET":
 			const removalIndex = state.pets.findIndex(pet => pet.id === action.id);
-			return Object.assign({}, state, {
-				pets: [
-				...state.pets.slice(0, removalIndex), ...state.pets.slice(removalIndex+1)
+			return { ...state, pets: [
+				...state.pets.slice(0, removalIndex), ...state.pets.slice(removalIndex + 1)
 				]
-			})	
+			};	
 		default:
 			return state;
 	}
 }
 
-export function dispatch(action){
+export const dispatch = (action) => {
 	state =  managePets(state, action)
-	render()
-}
+	render();
+};
 
 export const render = () => {
 	let container = document.getElementById('container');
-	
+	let petNames = state.pets.map((pet) => {
+		return `<li>${pet.name}</li>`;
+	}).join(' ');
+	container.innerHTML = `<ul>${petNames}</ul>`
 }
 
-dispatch({type: '@@init' })
