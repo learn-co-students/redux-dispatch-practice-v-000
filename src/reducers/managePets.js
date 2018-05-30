74fbf7ff9cf7c1c15067520337cd47d5463e9f3a
Allyson Wesman <allysonwesman@gmail.com>
2018-05-30 10:19:33 -0700

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,30 @@
 export let state;
 
-
-export function managePets(){
+export function managePets(state = {pets: []}, action) {
+  switch (action.type) {
+    case 'ADD_PET':
+      return {pets: [...state.pets, action.pet]};
+    case 'REMOVE_PET':
+      return {pets: state.pets.filter(pet => pet.id !== action.id)};
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render();
 }
 
 export function render(){
+  const petList = state.pets;
+  let petHTML = '<ul>'
 
+  for (let pet of petList) {
+    petHTML += '<li>' + pet.name + '</li>';
+  }
+
+  petHTML += '</ul>'
+
+  document.getElementById('container').innerHTML = petHTML;
 }
