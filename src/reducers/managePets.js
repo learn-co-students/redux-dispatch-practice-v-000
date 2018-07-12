98f4efc32cdbb15af7ed94111dea4084e267f3d9
bretburau <github email address>
2018-07-11 23:06:01 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,34 @@
+import { stat } from "fs";
+
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return {pets: [...state.pets, action.pet ]}
+    case 'REMOVE_PET':
+      return {
+        pets: state.pets.filter(function(pet){
+          return pet.id !== action.id
+        })
+      }
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render();
 }
 
 export function render(){
-
+  let content = '<ul>'
+  state.pets.forEach(pet => {
+    content += `<li>${pet.name}</li>`
+  });
+  content += '</ul>'
+  console.log(content)
+  document.getElementById('container').innerHTML = content
 }
