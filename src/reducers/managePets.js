e48f152574417bdf0769491c6d23bc7ba87a1ed6
gringobrasiliero <nolanehs@gmail.com>
2018-07-14 07:55:00 -0400

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,39 @@
 export let state;
 
 
-export function managePets(){
-}
+export function managePets(state = {
+  pets: [], }, action){
 
-export function dispatch(){
+  switch (action.type) {
+    case 'ADD_PET':
+    return {...state, pets: [
+      ...state.pets, action.pet
+    ]}
+  
+
+    case 'REMOVE_PET':
+    const removeThisIndex = state.pets.findIndex(pet => pet.id === action.id)
+    return {...state, pets: [
+      ...state.pets.slice(0, removeThisIndex),
+      ...state.pets.slice(removeThisIndex +1)
+    ]}
+    default:
+  return state
+
+  }
+  }
+  
+
+
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let container = document.getElementById('container')
+  let petsList = state.pets.map(pet =>
+    `<li>${pet.name}</li>`)
+    .join(' ');
+  container.innerHTML = `<ul>${petsList}</ul>`
 }
