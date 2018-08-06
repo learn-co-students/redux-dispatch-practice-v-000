029871f79431118a803f80a1d14cceda24904401
TylerP33 <tyler3321@gmail.com>
2018-08-06 15:34:44 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,32 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = { pets: [] }, action){
+	switch (action.type) {
+
+		case 'ADD_PET':
+			return {pets: [
+				...state.pets, action.pet
+				]}
+
+		case 'REMOVE_PET':
+			return {pets: state.pets.filter(pets => pets.id !== 101)}
+
+		default:
+			return state;
+	}
 }
 
-export function dispatch(){
+export function dispatch(action){
+    state = managePets(state, action)
+    render()
 }
 
 export function render(){
-
+  let container = document.getElementById('container')
+  let pets = state.pets.map(pet =>
+    `<li>${pet.name}</li>`)
+    .join(' ');
+  container.innerHTML = `<ul>${pets}</ul>`
 }
+
