e5c04efd2e698ac80930ba20ed7516d0e7f828bd
Jed Peek <jedpeek@Jeds-MacBook-Pro.local>
2018-09-24 17:16:36 -0700

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,60 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action){
+  switch (action.type){
+    case 'ADD_PET':
+      return {pets: [...state.pets, action.pet]}
+      case "REMOVE_PET":
+        const removalIndex = state.pets.findIndex(pet => pet.id === action.id);
+        return (
+          {...state,
+              pets: [
+                ...state.pets.slice(0, removalIndex),
+                ...state.pets.slice(removalIndex + 1)
+              ]
+          }
+        )
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  const container = document.getElementById("container")
+  const petsList = state.pets.map((pet)=> {
+    return `<ul><li>${pet.name}</li></ul>`
+  }).join(' ');
+  console.log(petsList)
+  container.innerHTML = petsList
 }
+
+
+// let state;
+//
+// function changeState(state = { count: 0 }, action) {
+//     switch (action.type) {
+//
+//       case 'INCREASE_COUNT':
+//         return { count: state.count + 1 }
+//
+//       default:
+//         return state;
+//     }
+//   }
+//
+//   function dispatch(action){
+//     state = changeState(state, action)
+//     render()
+// }
+//
+// function render(){
+//     document.innerHTML = state.count
+// }
+//
+// dispatch({type: '@@INIT'})
