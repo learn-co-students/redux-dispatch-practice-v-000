80de2416993bed3630d9a11f8ce71c8c7298b837
Suchitra Naik <susanaik@gmail.com>
2018-10-18 16:32:14 +0000

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,38 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state={pets: []}, action){
+
+  switch(action.type){
+    case 'ADD_PET':
+      return ({...state,
+                  pets: [
+                    ...state.pets,
+                    action.pet
+                  ]
+              });
+    case 'REMOVE_PET':
+    const removalIndex = state.pets.findIndex(pet => pet.id === action.id);
+    return (
+             {...state,
+               pets: [
+               ...state.pets.slice(0, removalIndex),
+               ...state.pets.slice(removalIndex + 1)
+               ]
+             }
+          )
+    default:
+      return state;
+  }
+
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action);
+  render();
 }
 
 export function render(){
+  let container = document.getElementById('container')
 
 }
