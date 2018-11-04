455df915341b11ab4cb19fb40c4ef93b775c8fdb
Jrodr4544 <maskdbandit@gmail.com>
2018-11-04 01:37:56 +0000

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -31,15 +31,3 @@ export function render(){
 	document.getElementById("container").innerHTML = html
 }
 
-
-//Overview
-
-//Ever have that feeling where you are just surrounded by so many of your pets that you start to lose track of all of them. Well now there's an app for that-- or there will be-- you're going to build it! By dispatching actions such as "ADD_PET" and "REMOVE_PET" to your reducer, you will track your current pets and display them in a beautiful <ul> in the browser.
-//Instructions
-
-//In managePets.js you need to write three functions managePets, dispatch, and render.
-
-//    The managePets reducer function needs to have a sensible default state
-//    dispatch should pass an action to the reducer and use that return value to update the state, a globally accessible variable
-//    Since our users want to see their pets on a webpage we want to have a render method that inserts a <ul> to the DOM with each pet's name wrapped in an <li>. The <ul> should be a child of an element with the id of container. There's no need to load jQuery into our app for such a small task. We can make use of built-in JavaScript methods like document.getElementById.
-
