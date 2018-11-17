57a5c8fb0aeb997affd99200360ba0539ac28018
github username <schun08@gmail.com>
2018-11-17 14:52:12 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,26 +1,55 @@
+// first attempt 5/21/18
+// export let state;
+//
+//
+// export function managePets(state = {pets: []}, action){
+//   switch(action.type){
+//     case 'ADD_PET':
+//       return {pets: [...state.pets, action.pet]}
+//     case 'REMOVE_PET':
+//       const remove = state.pets.findIndex(pet => pet.id === action.id)
+//       return {pets: [...state.pets.slice(0, remove), ...state.pets.slice(remove+1)]}
+//     default:
+//       return state;
+//   }
+// }
+//
+// export function dispatch(action){
+//   state = managePets(state, action);
+//   render();
+// }
+//
+// export function render(){
+//   const pets = state.pets.map(pet => {
+//     return `<li>${pet.name}</li>`
+//   }).join(' ')
+//   document.getElementById('container').innerHTML = `<ul>${pets}</ul>`
+// }
+
+// second attempt 11/17/18
 export let state;
 
 
-export function managePets(state = {pets: []}, action){
-  switch(action.type){
+export function managePets(state={pets: []}, action){
+  switch (action.type) {
     case 'ADD_PET':
       return {pets: [...state.pets, action.pet]}
     case 'REMOVE_PET':
-      const remove = state.pets.findIndex(pet => pet.id === action.id)
-      return {pets: [...state.pets.slice(0, remove), ...state.pets.slice(remove+1)]}
+      const id = state.pets.findIndex(p => p.id === action.id)
+      return {pets: [...state.pets.slice(0, id), ...state.pets.slice(id+1)]}
     default:
-      return state;
+      return state
   }
-}
+};
 
 export function dispatch(action){
   state = managePets(state, action);
   render();
-}
+};
 
 export function render(){
-  const pets = state.pets.map(pet => {
-    return `<li>${pet.name}</li>`
-  }).join(' ')
-  document.getElementById('container').innerHTML = `<ul>${pets}</ul>`
-}
+    const pets = state.pets.map(pet => {
+      return `<li>${pet.name}</li>`
+    }).join(' ')
+    document.getElementById('container').innerHTML = `<ul>${pets}</ul>`
+};
